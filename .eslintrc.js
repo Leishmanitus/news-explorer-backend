bb86f71eba5397b7292719d5767f7e45a0e5fced
Tyler Leishman <tyler.leishman0@gmail.com>
2024-07-26 12:23:48 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: ["eslint:recommended", "airbnb-base", "prettier"],
+    overrides: [
+      {
+        env: {
+          node: true,
+        },
+        files: [".eslintrc.{js,cjs}"],
+        parserOptions: {
+          sourceType: "script",
+        },
+      },
+    ],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "no-underscore-dangle": [ "error", { "allow": [ "_id" ] } ],
+      "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+    },
+  };
